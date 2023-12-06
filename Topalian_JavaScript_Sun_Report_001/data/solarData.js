2ddb5abdb34c667c3a6b24a8c5bd0d0331512408
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-12-06 07:20:25 -0500

    Add files via upload

---
+++ Topalian_JavaScript_Sun_Report_001/data/solarData.js
@@ -0,0 +1,49 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// https://gitlab.com/ChristopherAndrewTopalian
+
+// solarData.js
+
+let width001 = 400;
+let height001 = 400;
+
+let solarData =
+[
+    {
+        name: "noaa_sun_now_red",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg",
+        description: "Current Image of the Sun - Red",
+        theWidth: width001,
+        theHeight: height001
+    },
+    
+    {
+        name: "noaa_sun_now_orange",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0193.jpg",
+        description: "Current Image of the Sun - Orange",
+        theWidth: width001,
+        theHeight: height001
+    },
+    
+    {
+        name: "noaa_sun_now_magnetogram",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIB.jpg",
+        description: "Current Image of the Sun - Magnetogram",
+        theWidth: width001,
+        theHeight: height001
+    },
+    
+    {
+        name: "noaa_sun_now_Intensitygram",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIIF.jpg",
+        description: "Current Image of the Sun - Intensitygram \nSun Spots",
+        theWidth: width001,
+        theHeight: height001
+    }
+];

58a0848083c1b79490388bd09ee8c7e52f2d5453
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-12-17 16:48:58 -0500

    Add files via upload

---
+++ Topalian_JavaScript_Sun_Report_002/data/solarData.js
@@ -0,0 +1,143 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// solarData.js
+
+let width001 = 400;
+let height001 = 400;
+
+let solarData =
+[
+    {
+        name: "LASCO/C3",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_c3.gif",
+        description: "Animated Gif from LASCO/C3",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "LASCO/C2",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_c2.gif",
+        description: "Animated Gif from LASCO/C2",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "LASCO C2",
+        url: "https://soho.nascom.nasa.gov/data/realtime/c2/512/latest.jpg",
+        description: "SOHO LASCO C2 Latest Image",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "noaa_sun_now_red",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.jpg",
+        description: "Current Image of the Sun - Red",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "noaa_sun_now_orange",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0193.jpg",
+        description: "Current Image of the Sun - Orange",
+        theWidth: width001,
+        theHeight: height001
+    },
+    
+    {
+        name: "noaa_sun_now_magnetogram",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIB.jpg",
+        description: "Current Image of the Sun - Magnetogram",
+        theWidth: width001,
+        theHeight: height001
+    },
+    
+    {
+        name: "noaa_sun_now_Intensitygram",
+        url: "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_HMIIF.jpg",
+        description: "Current Image of the Sun - Intensitygram \nSun Spots",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "Nasa Sunspots",
+        url: "https://soho.nascom.nasa.gov/data/synoptic/sunspots_earth/mdi_sunspots_1024.jpg",
+        description: "Sun Spots",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "NOAA Aurora Forecast-Northern Hemisphere",
+        url: "https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg",
+        description: "Aurora Forecast - North",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "NOAA Aurora Forecast-Southern Hemisphere",
+        url: "https://services.swpc.noaa.gov/images/animations/ovation/south/latest.jpg",
+        description: "Aurora Forecast - South",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "Space Weather Overview",
+        url: "https://services.swpc.noaa.gov/images/swx-overview-small.gif",
+        description: "Overview",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "WSA-Enlil Prediction",
+        url: "https://services.swpc.noaa.gov/images/animations/enlil/latest.jpg",
+        description: "WSA-Enlil Prediction",
+        theWidth: 625,
+        theHeight: height001
+    },
+
+    {
+        name: "EIT 284A",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_eit_284.gif",
+        description: "Animated Gif from EIT 284A",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "EIT 195A",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_eit_195.gif",
+        description: "Animated Gif from EIT 195A",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "EIT 171A",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_eit_171.gif",
+        description: "Animated Gif from EIT 171A",
+        theWidth: width001,
+        theHeight: height001
+    },
+
+    {
+        name: "EIT 304A",
+        url: "https://soho.nascom.nasa.gov/data/LATEST/current_eit_304.gif",
+        description: "Animated Gif from EIT 304A",
+        theWidth: width001,
+        theHeight: height001
+    }
+];
